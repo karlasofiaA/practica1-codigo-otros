81705ed2f8ae39254f21dafa2522b94ca3bb949f
Karla Arenas <karlasofia.arenasv@gmail.com>
2023-02-03 20:52:11 -0600

    ci: Correccion de codigo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,24 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = 'https://api.github.com';  
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name'); //Se colocó un "."
+const $b = document.querySelector('.blog'); //Se cambió y se puso un "."
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { //Se agregó la palabra "async"
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  data = await response.json();  //Se agregó esta línea para usar la const response
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //En las siguientes tres líneas se cambia '' por ``
+  $b.textContent = `${data.blog}`;   
+  $n.textContent = `${data.name}`; 
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`; //Agregar signo $ del selector
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski').catch(handleError('stolinski')); //Mandar llamar a la función con () y parámetro
